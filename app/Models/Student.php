7effa3cdbb07c782df006c430ab54bed8df58ee3
Apurunggu <gepenghaikal384@gmail.com>
2024-11-03 21:01:50 +0700

    tugas praktikum6

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,21 +2,17 @@
 
 namespace App\Models;
 
-use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB; // Tambahkan ini
 
 class Student extends Model
 {
-
+    protected $fillable = ['nama', 'nim', 'email', 'jurusan'];
 
     public static function getAllStudents()
     {
+        // Menggunakan DB untuk menjalankan query langsung
         $students = DB::select('select * from students');
         return $students;
     }
-
-    protected $fillable = ['nama', 'nim', 'email', 'jurusan'];
 }
-
-
-
